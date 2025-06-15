510c7de93d6e004f288b6562690101d0f93e9a1b
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2025-06-15 11:56:55 -0300

    feat: going back to learn how to do the app alone

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -65,19 +65,16 @@ const Header = () => {
   );
 };
 
-const Pizza = ({ pizzaObj }) => {
-  console.log(pizzaObj);
-
+const Pizza = (props) => {
   return (
-    <li className={`pizza ${pizzaObj.soldOut ? "sould-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
       <div>
-        <h3>{pizzaObj.name}</h3>
-        <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.souldOut ? "SOLD OUT" : pizzaObj.price}</span>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
       </div>
-    </li>
+    </div>
   );
 };
 
@@ -88,23 +85,12 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
-          </p>
-
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>We're still working on our menu. Please come back later :)</p>
-      )}
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
     </main>
   );
 };
