b2be8cb0403edf95830c74acc2872a2322d81c78
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2025-06-16 12:28:27 -0300

    feat: upgrading the pizzas menu

--- pizza-menu/src/components/Pizza.js
+++ pizza-menu/src/components/Pizza.js
@@ -1,10 +1,14 @@
 const Pizza = (props) => {
   return (
     <li className={`pizza ${props.soldOut ? "sold-out" : ""}`}>
-      <img src={props.photoName} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
-      <span>{props.soldOut ? "SOLD OUT" : `$${props.price}`}</span>
+      <div className="pizza-details">
+        <img src={props.photoName} alt={props.name} />
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+      </div>
+      <span className="price">
+        {props.soldOut ? "SOLD OUT" : `$${props.price}`}
+      </span>
     </li>
   );
 };
